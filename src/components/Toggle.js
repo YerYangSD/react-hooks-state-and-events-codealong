ae11200acfd055160d111a5af436ac499cb05a14
Yer Yang <yyhmg_86@yahoo.com>
2023-08-18 23:34:39 -0700

    uses state variable in the component to toggles the background color of the button between red and white

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -5,7 +5,10 @@ function Toggle() {
   function handleClick() {
     setIsOn((isOn) => !isOn)
   }
-  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+
+  const color = isOn ? "red" : "white";
+
+  return <button style={{ background: color }} onClick={handleClick} > {isOn ? "ON" : "OFF"}</button >;
 }
 
 export default Toggle;
